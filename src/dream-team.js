c1e864a50532d10446a7d87e32c2fbddf4b10f36
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-24 21:40:16 +0300

    feat: add basic code by dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr = []; // Будущий массив массивов. Нужен для ввода внего массивов из букв имён
+  let team = []; // Массив заглавных букв имён
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === "string") arr.push(members[i].trim().split("")); // если это строка, чистим её от пробелов и добавляем её в массив разделив на массив букв
+    }
+    for (let member of arr) {
+      team.push(member[0].toUpperCase()); //делаем первую букву заглавной и добавляем в массив букв имен
+    }
+    return team.sort().join(""); // сортируем и переводим в строку
+  } else return false;
 };
