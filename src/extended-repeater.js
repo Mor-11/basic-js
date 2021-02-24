968f1301c140c166f4bd678e7d004b90fe85d74b
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-24 21:41:36 +0300

    feat: add basic code by extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let repeat = 1;
+  if (options.repeatTimes !== undefined)  repeat = options.repeatTimes;
+  let string = "+"; 
+  if (options.separator !== undefined) string = options.separator; 
+  let stringStr = ""; 
+  if (options.addition !== undefined) stringStr = options.addition; 
+  let repeatStr = 1;
+  if (options.additionRepeatTimes !== undefined)
+  repeatStr = options.additionRepeatTimes; 
+  let separatorStr = "|"; 
+  if (options.additionSeparator !== undefined) separatorStr = options.additionSeparator; 
+  let newStr = "";
+  if ( repeat > 0) {
+  for (let i = 0; i <  repeat; i++) {
+  newStr += str;
+  for (let j = 0; j < repeatStr; j++) {
+  newStr += stringStr;
+  if (j < repeatStr - 1) newStr += separatorStr;
+  }
+  if (i <  repeat - 1) newStr += string;
+  }
+  }
+  return newStr;
+  };
   
