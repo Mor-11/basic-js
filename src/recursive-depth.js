c84e1dd70f1bf9dc1bec38634e40976cb7f33494
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-24 21:43:53 +0300

    feat: add recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let position = 1; //начальная глубина массива
+    if (Array.isArray(arr)) {
+      //проверка есть ли массив
+      for (let item of arr) {
+        // перебор массива
+        if (Array.isArray(item)) {
+          let newPosition = this.calculateDepth(item); //рекурсия
+          if (newPosition >= position) {
+            // проверка на позици
+            position = newPosition + 1;
+          }
+        }
+      }
+      return position;
+    } else return 0;
   }
 };
