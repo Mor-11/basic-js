4f04a52ddcce7929c2d82c2b6aa12997308154aa
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-24 21:47:50 +0300

    feat: add basic code by vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  direct = true;
+  constructor(type) {
+    if (type === false) {
+      this.direct = false;
+    }
+  }
+  encrypt(message, key) {
+    if (message == "" || key == "") throw new Error("lack of arguments");
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let Arr = [];
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (j == key.length) j = 0;
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        let ch = (message.charCodeAt(i) - 65 + key.charCodeAt(j) - 65) % 26;
+        Arr.push(String.fromCharCode(ch + 65));
+        j++;
+      } else {
+        Arr.push(message[i]);
+      }
+    }
+    if (!this.direct) Arr.reverse();
+    return Arr.join("");
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage == "" || key == "")
+      throw new Error("lack of arguments");
+    let message = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let array = [];
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (j == key.length) j = 0;
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        let ch2 =
+          (message.charCodeAt(i) - 65 - (key.charCodeAt(j) - 65) + 26) % 26;
+        array.push(String.fromCharCode(ch2 + 65));
+        j++;
+      } else {
+        array.push(message[i]);
+      }
+    }
+    if (!this.direct) array.reverse();
+    return array.join("");
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
